596892952038b9bd99d715a5487daa19ca9c4448
Rafa <rafaelacristina1510.oliveira@gmail.com>
2026-02-06 16:29:46 -0300

    Add files via upload

---
+++ ativ_confortovisual/script.js
@@ -0,0 +1,12 @@
+
+        function modoDark() {
+            document.getElementById("pagina").style.backgroundColor = "#121212";
+            document.getElementById("titulo").style.color = "#ffffff";
+            document.getElementById("texto").style.color = "#f5f5f5";
+        }
+
+        function modoLight() {
+            document.getElementById("pagina").style.backgroundColor = "#ffffff";
+            document.getElementById("titulo").style.color = "#000000";
+            document.getElementById("texto").style.color = "#000000";
+        }
