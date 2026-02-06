7c6ec9aa35438fdd99e72ce0bab3a08f78417eff
Rafa <rafaelacristina1510.oliveira@gmail.com>
2026-02-06 20:18:06 -0300

    Add files via upload

---
+++ atv_confortovisual2/script.js
@@ -0,0 +1,17 @@
+function modoDark() {
+    document.getElementById("pagina").style.backgroundColor = "#121212";
+    document.getElementById("titulo").style.color = "#ffffff";
+    document.getElementById("texto").style.color = "#f5f5f5";
+    document.getElementById("menu").style.backgroundColor = "#1e1e1e";
+    document.getElementById("rodape").style.backgroundColor = "#1e1e1e";
+    document.getElementById("rodape").style.color = "#ffffff";
+}
+
+function modoLight() {
+    document.getElementById("pagina").style.backgroundColor = "#ffffff";
+    document.getElementById("titulo").style.color = "#000000";
+    document.getElementById("texto").style.color = "#000000";
+    document.getElementById("menu").style.backgroundColor = "#f2f2f2";
+    document.getElementById("rodape").style.backgroundColor = "#f2f2f2";
+    document.getElementById("rodape").style.color = "#000000";
+}
